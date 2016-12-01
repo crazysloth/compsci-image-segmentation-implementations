b64b61093503ca62009b67b18d0c8090af54b20c
Jonas Debes <jonas.debes@auckland.ac.nz>
2016-12-01 18:46:13 +1300

    Adaptive Thresholding, Added plot to show different threshold points on original histogram

--- src/main/java/AdaptiveThresholding.java
+++ src/main/java/AdaptiveThresholding.java
@@ -1,8 +1,12 @@
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.Plot;
+import ij.gui.PlotWindow;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 /**
@@ -25,16 +29,27 @@ public class AdaptiveThresholding implements PlugInFilter {
             cumHistogram[i] = cumHistogram[i - 1] + histogram[i];
         }
 
+        PlotWindow.noGridLines = true;
+        Plot plot = new Plot("Orig Histo", "Intensity", "Freq", IntStream.range(0,histogram.length).mapToDouble(a -> a).toArray(), Arrays.stream(histogram).mapToDouble(a -> a).toArray());
+
         long thresh = LongStream.range(0, histogram.length).reduce(0, (sum, next) ->  sum + (next * histogram[(int) next])) / totalPixels;
 
         long threshNext = -1;
+        double[] yAxis = {0.0, Arrays.stream(histogram).mapToDouble(a -> a).max().getAsDouble()};
+
         while (thresh != threshNext) {
             IJ.log("threshold: " + thresh);
+            double[] xAxis = {(double) thresh, (double) thresh};
+            plot.addPoints(xAxis, yAxis, PlotWindow.X);
+            plot.addPoints(xAxis, yAxis, PlotWindow.LINE);
+
             long meanObj = LongStream.range(0, thresh).reduce(0, (sum, next) ->  sum + (next * histogram[(int) next])) / cumHistogram[(int) thresh];
             long meanBg = LongStream.range(thresh, histogram.length).reduce(0, (sum, next) ->  sum + (next * histogram[(int) next])) / (cumHistogram[histogram.length - 1] - cumHistogram[(int) thresh]);
             threshNext = thresh;
             thresh = (meanObj + meanBg) / 2;
         }
+
+        plot.show();
 
         for (int y = 0; y < ip.getHeight(); y++) {
             for (int x = 0; x < ip.getWidth(); x++) {
