48a743a0c5bffc396c9d43571b8ae9d0dbbcb34f
Jonas Debes <jpdebes@gmail.com>
2017-01-12 21:55:09 +1300

    Added Otsu method

---
+++ src/main/java/OtsuThresholding.java
@@ -0,0 +1,61 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+/**
+ * Created by jonas on 3/12/16.
+ */
+public class OtsuThresholding implements PlugInFilter {
+
+    public int setup(String arg, ImagePlus imagePlus) {
+        return DOES_16;
+    }
+
+    public void run(ImageProcessor ip) {
+        int[] histogram = ip.getHistogram();
+
+        int[] cumHistogram = new int[histogram.length];
+
+        long totalMeanSum = 0;
+
+        for (int i = 1; i < cumHistogram.length; i++) {
+            cumHistogram[i] = cumHistogram[i - 1] + histogram[i];
+            totalMeanSum += i * histogram[i];
+        }
+
+        long sumMeanObg = 0;
+        double maxVar = 0;
+        int optimalThresh = 0;
+
+        for (int i = 0; i < histogram.length - 1; i++) {
+            sumMeanObg += i * histogram[i];
+
+            if (sumMeanObg == 0) {
+                continue;
+            }
+
+            long sumMeanBg = totalMeanSum - sumMeanObg;
+            long bgPix = cumHistogram[cumHistogram.length -1] - cumHistogram[i];
+            double betweenVar = cumHistogram[i] * bgPix * Math.pow(((sumMeanObg/cumHistogram[i]) - (sumMeanBg/bgPix)), 2);
+
+            if (betweenVar > maxVar) {
+                maxVar = betweenVar;
+                optimalThresh = i;
+            }
+        }
+
+        IJ.log("threshold: " + optimalThresh);
+
+        for (int y = 0; y < ip.getHeight(); y++) {
+            for (int x = 0; x < ip.getWidth(); x++) {
+                if (ip.getPixel(x,y) <= optimalThresh) {
+                    ip.putPixel(x,y,0);
+                } else {
+                    ip.putPixel(x,y, histogram.length - 1);
+                }
+            }
+        }
+    }
+
+}
