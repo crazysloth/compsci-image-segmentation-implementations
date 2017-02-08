c16a62849cab933e7aed6b33909023de2956df95
Jonas Debes <jpdebes@gmail.com>
2017-02-09 01:11:23 +1300

    Added ability to open stacks for debugging

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,8 @@
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
+import ij.ImageStack;
+import ij.plugin.FolderOpener;
 
 /**
  * Created by jonas on 29/11/16.
@@ -8,15 +10,19 @@ import ij.ImagePlus;
 public class Application {
 
     public static void main(String[] args) {
-        Class<?> clazz = AdaptiveThresholding.class;
+        Class<?> clazz = SeededRegionGrowing.class;
         String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
         String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
         System.setProperty("plugins.dir", pluginsDir);
 
         new ImageJ();
 
-        ImagePlus image = IJ.openImage("/home/jonas/Pictures/Soil_slices0000.tif");
+        ImagePlus seedImage = IJ.openImage("/home/jonas/Documents/Seeds.tif");
+        seedImage.show();
+        //ImagePlus image = IJ.openImage("/home/jonas/Pictures/Soil_slices0000.tif");
+        ImagePlus image = FolderOpener.open("/home/jonas/Documents/soil-samples");
         image.show();
+        IJ.selectWindow("soil-samples");
 
         IJ.runPlugIn(clazz.getName(), "");
     }
